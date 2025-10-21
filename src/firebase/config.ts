208c87e93ab9dd6e34ee040f684c80532910a430
Adauto Bueno <adauto.bueno@gmail.com>
2025-10-21 14:18:53 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,9 @@
-// @ts-nocheck
-// TODO: Add your Firebase project configuration object here.
-export const firebaseConfig = null;
+
+export const firebaseConfig = {
+  projectId: "studio-47897999-47232",
+  appId: "1:375773843007:web:afb66691507e839db7328a",
+  apiKey: "AIzaSyB7NNwX7XyTVCM8BfUx22GP51DjLzzbXmI",
+  authDomain: "studio-47897999-47232.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "375773843007",
+};
